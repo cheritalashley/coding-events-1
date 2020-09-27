711a17796171e85b827f03afbe720ff7ecfc7109
Cherita <lashleynook@gmail.com>
2020-09-26 22:26:32 -0500

    completed exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -7,20 +7,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
-/**
- * Created by Chris Bay
- */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    //private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
+
+   /* @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", events);
+        return "events/index";
+    }*/
 
     @GetMapping
-    public String displayAllEvents(Model model) {
+    public String displayEventsAndDescription(Model model){
         model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
@@ -33,8 +37,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String description) {
+        events.put(eventName, description);
         return "redirect:";
     }
 
